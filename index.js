1955b5c08ad076f527a3534a96224a7f1cdc7d61
Johan <tosojohan@gmail.com>
2020-11-21 03:24:33 +0100

    Footer : Icon+Tooltip+Responsive

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-function sendContact() {
-    var valid;
-    valid = validateContact();
-    if (valid) {
-        jQuery.ajax({
-            url: "contact_mail.php",
-            data: 'userName=' + $("#userName").val() + '&email=' +
-                $("#email").val() + '&exampleFormControlTextarea1=' +
-                $("#exampleFormControlTextarea1").val(),
-            type: "POST",
-            success: function (data) {
-                $("#mail-status").html(data);
-            },
-            error: function () {}
-        });
-    }
-}
-
-function validateContact() {
-    var valid = true;
-    if (!$("#userName").val()) {
-        $("#userName").css('background-color', '#FFFFDF');
-        valid = false;
-    }
-    if (!$("#email").val()) {
-        $("#email").css('background-color', '#FFFFDF');
-        valid = false;
-    }
-    if (!$("#email").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
-        $("#email").css('background-color', '#FFFFDF');
-        valid = false;
-    }
-    return valid;
-}
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+  });
