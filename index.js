3fa751a3c0100869d65bef9c05f0881909398cb9
Johan <tosojohan@gmail.com>
2020-11-23 20:03:58 +0100

    add typedJS+delete old files

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import LocomotiveScroll from 'locomotive-scroll';
+import Typed from 'typed.js';
+
+// LOCOMOTIVE SCROLL
 
 const scroll = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
@@ -6,7 +9,26 @@ const scroll = new LocomotiveScroll({
 });
 console.log('ok');
 
+
+// TOOLTIP BOOTSTRAP
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
   });
+
+
+// TYPED JS
+
+var typed = new Typed('.animate-title', {
+    strings: [
+      "",
+      "A COOL DEVELOPER",
+      "A CURIOUS DEVELOPER",
+      "YOUR NEXT DEVELOPER"
+    ],
+    typeSpeed: 100,
+    typeSpeed: 100,
+    typeSpeed: 100,
+    loop: false,
+  });
+
  
