fa8f53cf876b6acdb9bbe4be544ee4c66fc22667
Johan <tosojohan@gmail.com>
2020-11-23 19:48:25 +0100

    add pkgjson + smooth scroll + locomotive scoll

--- contactForm.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $toEmail = "tosojohan@gmail.com";
-    $mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["email"] .">\r\n";
-    if(mail($toEmail, $_POST["exampleFormControlTextarea1"])) {
-        print "<p class='success'>Mail Sent.</p>";
-    } else {
-        print "<p class='Error'>Problem in Sending Mail.</p>";
-    }
-?>
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
+console.log('ok');
+
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
   });
+ 
